bc6c496b80ba7097c0b68f3f211c6211a86d47a6
Dmitry Tsvirkun <dmitry@beamr.com>
2021-06-25 14:55:40 +0300

    homework for lesson2

--- geekshop/views.py
+++ geekshop/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+import json
+
+
+def parse(menu_json):
+    with open(menu_json,'r', encoding='utf-8') as f:
+        menu = json.load(f)
+    return menu['links']
+
 
 def index(request):
-    return render(request, 'geekshop/index.html')
+    menu = parse('data.json')
+    context = {'menu': menu}
+    return render(request, 'geekshop/index.html', context)
 
-def  contacts(request):
-    return render(request, 'geekshop/contact.html')
+
+def contacts(request):
+    menu = parse('data.json')
+    context = {'menu': menu}
+    return render(request, 'geekshop/contact.html', context)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from geekshop.views import parse
+
+
+prod_menu = [
+    {'href': '#', 'name': 'все'},
+    {'href': '#', 'name': 'дом'},
+    {'href': '#', 'name': 'офис'},
+    {'href': '#', 'name': 'модерн'},
+    {'href': '#', 'name': 'классика'},
+]
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    menu = parse('data.json')
+    context = {
+        'menu': menu,
+        'prod_menu': prod_menu
+    }
+    return render(request, 'mainapp/products.html', context)
