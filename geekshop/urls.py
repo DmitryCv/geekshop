ce37392909d9fbce27d91fc839ccfd6bc2c19382
Dmitry Tsvirkun <dmitry@beamr.com>
2021-07-04 16:26:32 +0300

    homework for lesson4

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -6,6 +6,7 @@ from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('authapp.urls', namespace='auth')),
     path('', index, name='index'),
     path('contacts/', contacts, name='contacts'),
     path('products/', include('mainapp.urls'))
